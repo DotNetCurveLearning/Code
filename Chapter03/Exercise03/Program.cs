79285693b353ac3f50e745b2b6d743e891e99bec
Ernesto Antonio Rodriguez Acosta <ernestoantonio.rodriguezacosta@msc.com>
2023-01-09 17:32:04 +0100

    Test exercises

---
+++ Chapter03/Exercise03/Program.cs
@@ -0,0 +1,32 @@
+﻿using static System.Console;
+
+/*
+Replace any number divisible by three with the word fizz, 
+any number divisible by five with the word buzz, 
+and any number divisible by both with fizzbuzz. 
+*/
+
+for (int i = 1; i <= 100; i++)
+{
+    //string result = i switch {
+    //    i % 3 == 0b0 => i.ToString()
+    //};
+    if (i%3 == 0 && i%5 == 0)
+	{
+		Console.Write("Fizzbuzz");
+	}
+	else if (i % 3 == 0)
+    {
+        Console.Write("Fizz");
+    }
+    else if (i % 5 == 0)
+    {
+        Console.Write("Buzz");
+    }
+    else
+    {
+        Console.Write(i);
+    }
+
+    Console.Write(i <= 99 ? ", " : "");
+}
--- Chapter03/HandlingExceptions/Program.cs
+++ Chapter03/HandlingExceptions/Program.cs
@@ -1,2 +1,37 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+using static System.Console;
+
+//WriteLine("Before parsing");
+//Write("What is your age? ");
+
+//string? input = Console.ReadLine();
+
+//try
+//{
+//	int age = int.Parse(input);
+//    WriteLine($"You are {age} years old.");
+//}
+//catch (Exception ex)
+//{
+//    WriteLine($"{ex.GetType()} says {ex.Message}");
+//}
+//WriteLine("After parsing");
+
+// --- Catching with filters
+// We can add filters to a catch statement using the "when" keyword.
+Write("Enter an amount: ");
+string? amount = Console.ReadLine();
+
+try
+{
+	decimal amountValue = decimal.Parse(amount);
+}
+catch (FormatException) when (amount.Contains("$"))
+{
+
+    WriteLine("Amounts cannot use the dollar sign!");
+}
+catch (FormatException)
+{
+    WriteLine("Amounts must only contains digits!");
+}
