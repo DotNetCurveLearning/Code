5ab11338b357c54b06961e5d1140cabdbd972cab
Ernesto Antonio Rodriguez Acosta <ernestoacostabaracoa@xn--mail-92a.com>
2023-01-09 19:19:14 +0100

    Fixing workspace file. Ex03 done with switch expressions.

--- Chapter03/Exercise03/Program.cs
+++ Chapter03/Exercise03/Program.cs
@@ -1,16 +1,23 @@
 ﻿using static System.Console;
 
 /*
-Replace any number divisible by three with the word fizz, 
-any number divisible by five with the word buzz, 
-and any number divisible by both with fizzbuzz. 
+Replace any number divisible by three with the word fizz,
+any number divisible by five with the word buzz,
+and any number divisible by both with fizzbuzz.
 */
 
 for (int i = 1; i <= 100; i++)
 {
-    //string result = i switch {
-    //    i % 3 == 0b0 => i.ToString()
-    //};
+    string result = i switch {
+       int n when (n % 3 == 0 && n % 5== 0) => i < 99 ? "Fizzbuzz, " : "Fizzbuzz",
+       int n when (n % 3 == 0 ) => i < 99 ? "Fizz, " : "Fizz",
+       int n when (n % 5 == 0 ) => i < 99 ? "Buzz, " : "Buzz",
+       _ => $"{i}, "
+    };
+
+    Write(result);
+
+    /*
     if (i%3 == 0 && i%5 == 0)
 	{
 		Console.Write("Fizzbuzz");
@@ -29,4 +36,7 @@ for (int i = 1; i <= 100; i++)
     }
 
     Console.Write(i <= 99 ? ", " : "");
+    */
 }
+
+WriteLine();
