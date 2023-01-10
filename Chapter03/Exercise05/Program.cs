b0e9a88e0f3d2e5c0f4cab265dd9ed3f4d8df97b
Ernesto Antonio Rodriguez Acosta <ernestoantonio.rodriguezacosta@msc.com>
2023-01-10 14:56:14 +0100

    Exercises chapter03

--- Chapter03/Exercise05/Program.cs
+++ Chapter03/Exercise05/Program.cs
@@ -1,2 +1,7 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using static System.Console;
+
+int x = 3;          // x is 4 
+int y = 2 + ++x;    // y is 6
+
+WriteLine(x);
+WriteLine(y);
