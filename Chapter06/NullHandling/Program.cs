84292cc909b3c49db4165d3d5443b020bd019634
Ernesto Antonio Rodriguez Acosta <ernestoantonio.rodriguezacosta@msc.com>
2023-01-27 14:13:42 +0100

    Dealing with nullable value types and reference types

---
+++ Chapter06/NullHandling/Program.cs
@@ -0,0 +1,30 @@
+﻿using static System.Console;
+
+// Working with null values
+
+//int thisCannotBeNulll = 4;
+//// thisCannotBeNulll = null;   // compile error!
+
+//int? thisCouldBeNulll = null;
+//WriteLine(thisCouldBeNulll);
+//WriteLine(thisCouldBeNulll.GetValueOrDefault());
+
+//thisCouldBeNulll = 7;
+//WriteLine(thisCouldBeNulll);
+//WriteLine(thisCouldBeNulll.GetValueOrDefault());
+
+// Declaring non-nullable variables and parameters
+
+Address address = new();
+address.Building = null;
+address.Street = null;
+address.City = "London";
+address.Region = null;
+class Address
+{
+    public string? Building;
+    public string Street = string.Empty;
+    public string City = string.Empty;
+    public string Region = string.Empty;
+}
+
