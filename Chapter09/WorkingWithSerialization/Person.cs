635fa7f3dffad8c81361d3b77a40c6b2c91787c5
Ernesto Antonio Rodriguez Acosta <ernestoantonio.rodriguezacosta@msc.com>
2023-03-14 17:29:10 +0100

    Serializing as XML

---
+++ Chapter09/WorkingWithSerialization/Person.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Packt.Shared
+{
+    public class Person
+    {
+
+        public Person()
+        {
+
+        }
+
+        public string? FirstName { get; set; }
+        public string? LastName { get; set; }
+        public DateTime DateOfBirth { get; set; }
+        public HashSet<Person> Children { get; set; }
+        public decimal Salary { get; set; }
+
+        public Person(decimal initialSalary)
+        {
+            initialSalary = initialSalary;
+        }
+    }
+}
--- Chapter09/WorkingWithSerialization/Program.cs
+++ Chapter09/WorkingWithSerialization/Program.cs
@@ -1,18 +1,59 @@
 ﻿using static System.Console;
+using static System.Environment;
+using static System.IO.Path;
+using Packt.Shared;
+using System.Xml.Serialization; // XMLSerializer
+using System.Text;
 
-namespace Packt.Shared;
-
-public class Person
+// creating an object graph of Person instances
+List<Person> people = new()
 {
+    new(3000M)
+    {
+        FirstName = "Alice",
+        LastName = "Smith",
+        DateOfBirth = new(1974, 3, 14)
+    },
+    new(4000M)
+    {
+        FirstName = "Bob",
+        LastName = "Jones",
+        DateOfBirth = new(1969, 11, 23)
+    },
+    new(20000M)
+    {
+        FirstName = "Charlie",
+        LastName = "Cox",
+        DateOfBirth = new(1984, 5, 4),
+        Children = new()
+        {
+            new(0M)
+            {
+                FirstName = "Sally",
+                LastName = "Cox",
+                DateOfBirth = new(2000, 7, 12)
+            }
+        }
+    }
+};
 
-	public string? FirstName { get; set; }
-	public string? LastName { get; set; }
-	public DateTime DateOfBirth { get; set; }
-	public HashSet<Person> Children { get; set; }
-	public decimal Salary { get; set; }
+// create object that will format a list of Persons as XML
+XmlSerializer xmlSerializer = new(people.GetType());
 
-	public Person(decimal initialSalary)
-	{
-		initialSalary = initialSalary;
-	}
+// create a file to write to
+string path = Combine(CurrentDirectory, "people.xml");
+
+using (FileStream stream = File.Create(path))
+{
+    // serialize the object graph to the stream
+    xmlSerializer.Serialize(stream, people);
 }
+WriteLine("Written {0:N0} bytes of XML to {1}", arg0: new FileInfo(path).Length, arg1: path);
+WriteLine();
+
+// display the serialized object graph
+WriteLine(File.ReadAllText(path));
+
+
+
+
